968eef1933bf51bcba2419234089ce842a3119b0
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-05-30 17:59:20 -0700

    first commit

---
+++ src/InnReservations.java
@@ -0,0 +1,13 @@
+public class InnReservations {
+
+
+
+    public static void main(String[] args ) {
+        // So we need to create an MVC and use design patterns that will be useful for what we are trying to do
+
+        // 1) First on main need to open a connection to the database
+        // 2) create and populate tables
+        // 3) Pull data from database with a sql statement and display this data
+        // 4) Will need to look at documentation to see how this data is returned and what data structure they are using
+    }
+}
